1cea6ca11c165e617a711305adae5518ca837eb2
Precious Humphrey <phumphrey23@gmail.com>
2018-10-29 20:57:33 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,4 @@
-class CreateHauntedHouse < ActiveRecord::Migration[5.1]
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_house do |t|
       t.string :name
