be7f8b74b6cdbaab452da6941ca241a986fe3512
Precious Humphrey <phumphrey23@gmail.com>
2018-10-29 20:29:46 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,4 +1,4 @@
-class CreateCostumes < ActiveRecord::Migration[]
+class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
@@ -9,8 +9,3 @@ class CreateCostumes < ActiveRecord::Migration[]
     end
   end
 end
-
-
-# Define a change method in which to do the migration
-# In this change method, create columns with the correct names and
-# value types according to the spec
